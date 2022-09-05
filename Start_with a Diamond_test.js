7f573673381f7775a495915e1f7efd06fab67d3c
JoseSissa <53451389+JoseSissa@users.noreply.github.com>
2022-09-05 13:32:14 -0500

    Add files via upload

---
+++ Start_with a Diamond_test.js
@@ -0,0 +1,102 @@
+Feature('Start with a Diamond');
+
+Scenario('Buy a diamond', ({ I }) => {
+
+    const shapes = ["Round", "Oval", "Cushion", "Princess", "Emerald", "Pear", "Radiant", "Asscher", "Marquise"],
+        caratFrom = 2,
+        caratTo = 4,
+        priceFrom = 2000,
+        priceTo = 10000,
+        report = ["IGI", "GIA", "GCAL"];
+    // Checking the shape filter.
+    function checkShape() {
+        for(const shape of shapes) {
+            I.click(`.${shape.toLocaleLowerCase()}-shape`);
+            for(const elem of shapes) {
+                if(elem != shape) {
+                    I.dontSee(elem);
+                };
+            };
+            I.click(`.${shape.toLocaleLowerCase()}-shape`);
+        };
+    }
+    // Checking the carat filter.
+    async function checkCarat() {
+        I.fillField("#from_carat_value_input", caratFrom);
+        I.pressKey("Enter");
+        I.fillField("#to_carat_value_input", caratTo);
+        I.pressKey("Enter");
+        const carat = await I.grabTextFromAll('tbody tr td:nth-child(3)');
+        for (const elem of carat) {
+            if(parseFloat(elem) < parseFloat(caratFrom) || parseFloat(elem) > parseFloat(caratTo)) {
+                console.log('Error in the values obtained from the Carat filter');
+            }
+        }
+    };
+    // Checking the price filter
+    async function checkPrice() {
+        I.fillField("#from_price_value_input", priceFrom);
+        I.pressKey('Enter');
+        I.fillField("#to_price_value_input", priceTo);
+        I.pressKey('Enter');
+        const prices = await I.grabTextFromAll('tbody tr td:nth-child(1)')
+        for (const elem of prices) {
+            console.log(Number((elem.slice(elem.indexOf('$')+1)).replace(',', '')));
+            if(((elem.slice(elem.indexOf('$')+1)).replace(',', '')) < priceFrom || ((elem.slice(elem.indexOf('$')+1)).replace(',', '')) > priceTo) {
+                console.log('Error in the values obtained from the Price filter');
+            }
+        }
+    }
+    // Check the Report filter
+    async function checkReport() {
+        let i = 1;
+        for (const elem of report) {
+            I.click(elem, `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i}) label`);
+            // pause();
+            I.click(elem, `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i}) label`);
+            // pause();
+            i++;
+        }
+        
+        // for(let i = 1; i < 4; i++) {
+        //     I.click(`#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i}) label`);
+        //     const results = await I.grabTextFromAll('tbody tr td:nth-child(7)');
+        //     for (const elem of results) {
+        //         console.log(elem);
+        //         // if(elem != report[i]) {
+        //         //     console.log('Error in the values obtained from the Report filter');
+        //         // }
+        //     }
+        //     I.click(`#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i}) label`);
+        // }
+    }
+    
+
+    
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+    // ----------------------------------------------------
+
+    I.amOnPage("/");
+    I.forceClick("Start With a Diamond");
+    I.seeInCurrentUrl("/engagement-ring/create/diamond");
+
+    // I.click('.round-shape');
+
+    // checkShape();
+    //------------------------------------------------------------------------------
+    // checkCarat();
+    //------------------------------------------------------------------------------
+    // checkPrice();
+    //------------------------------------------------------------------------------
+    I.click("#advanced_filters_button");
+    checkReport();
+    
+
+
+
+    // I.click(".oval-shape")
+    // I.dontSee("Round")
+    pause();
+    
+});
---
+++ codecept.conf.js
@@ -0,0 +1,27 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'https://novitadiamonds.com/',
+      // url: 'http://novitadiamonds.localhost',
+      // url: 'https://manmadediamonds.com.au',
+      show: true,
+      browser: 'chromium',
+      waitForAction: Number('1500'),
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'codecepJS'
+}
