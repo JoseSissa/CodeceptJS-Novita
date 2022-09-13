29ba3049ac81b9cfe70dbbdee415a6d295814a64
Jose <jsissame3484@universidadean.edu.co>
2022-09-13 17:13:57 -0500

    Ring design statirng filter test

--- Start-with-a-Diamond-test.js
+++ Start-with-a-Diamond-test.js
@@ -2,16 +2,19 @@ Feature('Start with a Diamond');
 
 Scenario('Buy a diamond', ({ I }) => {
 
-    
-    
-
     // Check the compare option
     function compareDiamonds() {
         I.click('//*[@id="body_table_results"]/tr[1]/td[9]/div/span[1]/img');
         I.click('//*[@id="body_table_results"]/tr[2]/td[9]/div/span[1]/img');
         I.click("#to_compare_diamonds_from_diamond_list");
     };
-    
+
+    function checkVideo() {
+        if(!(I.seeCssPropertiesOnElements('#diamond_detail_section .diamond_detail_tabs .video_tab', {'display':"none"}))) {
+            I.click('#diamond_detail_section .diamond_detail_tabs .video_tab');
+            I.see('Actual video of the diamond');
+        }
+    };
 
     // ----------------------------------------------------
     // Functions of metal types filters
@@ -110,108 +113,90 @@ Scenario('Buy a diamond', ({ I }) => {
     I.forceClick("Start With a Diamond");
     I.seeInCurrentUrl("/engagement-ring/create/diamond");
 
-    
-
     // I.scrollTo("#body_table_results", 0, 100);
-
     
-
-    
-
-    
-
-    
-    
-    
-    
-
-
-
-
     // TO COMPARE DIAMONDS
     //------------------------------------------------------------------------------
-    // compareDiamonds();
-    
-
-
-
-
-
-
+    I.wait(2);
+    compareDiamonds();
 
     // Choose Diamond
-    // I.click('//*[@id="body_table_comparison"]/tr[1]/td[9]/a/div'); //In the results table
+    I.click('//*[@id="body_table_comparison"]/tr[1]/td[9]/a/div'); //In the results table
     // I.click('//*[@id="body_table_results"]/tr[1]/td[10]/a/div');   //In the comparison table
     
     // Diamond detail
-    // I.see('Choose this diamond', 'a');
+    I.see('Choose this diamond', 'a');
+    pause();
     // Video
-    // I.click('#diamond_detail_section .diamond_detail_tabs .video_tab');
-    // I.see('Actual video of the diamond');
-    // Certificate
-    // I.click('#diamond_detail_section .diamond_detail_tabs .certificate_tab');
-    // I.switchToNextTab();
-    // I.seeInCurrentUrl('https://www.igi.org/reports/');
-    // I.closeCurrentTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
-    // I.click('#diamond_detail_section .diamond_detail_tabs .diamond_picture_tab');
+    checkVideo();
     
-    // I.click('#diamond_detail_section .diamond_detail_content_features .er_details_column_one a');
-    // I.switchToNextTab();
-    // I.seeInCurrentUrl('https://www.igi.org/reports/');
-    // I.closeCurrentTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
-    // I.click('20% Deposit Available');
-    // I.switchToNextTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/deposit');
-    // I.closeCurrentTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
-    // I.click('Choose this diamond');
-    // I.see('CREATE YOUR RING');
+    
+    // Certificate
+    I.click('#diamond_detail_section .diamond_detail_tabs .certificate_tab');
+    I.switchToNextTab();
+    I.seeInCurrentUrl('https://www.igi.org/reports/');
+    I.closeCurrentTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
+    I.click('#diamond_detail_section .diamond_detail_tabs .diamond_picture_tab');
+    
+    I.click('#diamond_detail_section .diamond_detail_content_features .er_details_column_one a');
+    I.switchToNextTab();
+    I.seeInCurrentUrl('https://www.igi.org/reports/');
+    I.closeCurrentTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
+    I.click('20% Deposit Available');
+    I.switchToNextTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/deposit');
+    I.closeCurrentTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
+    I.click('Choose this diamond');
+    I.see('CREATE YOUR RING');
 
     // Select Ring Design
-    // pause();
-    // I.forceClick('//*[@id="diamond_list_section"]/div[1]/div[2]/div[3]/div[2]/a');
-    // I.forceClick('#diamond_list_section .container_steps_title .step_2 .description_2 a');
+    pause();
+    I.forceClick('//*[@id="diamond_list_section"]/div[1]/div[2]/div[3]/div[2]/a');
+    I.forceClick('#diamond_list_section .container_steps_title .step_2 .description_2 a');
 
     // Filter metal type
-    // I.forceClick('#metal_type_1');
-    // checkAllMetal();
-    // I.forceClick('#metal_type_3');
-    // checkWhiteMetal();
-    // I.forceClick('#metal_type_3');
-    // I.forceClick('#metal_type_5');
-    // checkYellowMetal();
-    // I.forceClick('#metal_type_5');
-    // I.forceClick('#metal_type_4');
-    // checkRoseMetal();
-    // I.forceClick('#metal_type_4');
-    // I.forceClick('#metal_type_2');
-    // checkPlatinumMetal();
-    // I.forceClick('#metal_type_2');
+    I.forceClick('#metal_type_1');
+    checkAllMetal();
+    I.forceClick('#metal_type_3');
+    checkWhiteMetal();
+    I.forceClick('#metal_type_3');
+    I.forceClick('#metal_type_5');
+    checkYellowMetal();
+    I.forceClick('#metal_type_5');
+    I.forceClick('#metal_type_4');
+    checkRoseMetal();
+    I.forceClick('#metal_type_4');
+    I.forceClick('#metal_type_2');
+    checkPlatinumMetal();
+    I.forceClick('#metal_type_2');
     
 
     // Filter Price
-    // I.forceClick('#setting_price_range_2');
-    // checkPrice1000andUnder();
-    // I.forceClick('#setting_price_range_2');
-    // I.forceClick('#setting_price_range_3');
-    // checkPrice1000To2000();
-    // I.forceClick('#setting_price_range_3');
-    // I.forceClick('#setting_price_range_4');
-    // checkPrice2000andOver();
-    // I.forceClick('#setting_price_range_4');
-    // Search input
-    // searchInputRingDesign();
-    // pause();
-    // I.click('#ring_list_section .ring_item .ring_detail_link');
+    I.forceClick('#setting_price_range_2');
+    checkPrice1000andUnder();
+    I.forceClick('#setting_price_range_2');
+    I.forceClick('#setting_price_range_3');
+    checkPrice1000To2000();
+    I.forceClick('#setting_price_range_3');
+    I.forceClick('#setting_price_range_4');
+    checkPrice2000andOver();
+    I.forceClick('#setting_price_range_4');
+
+    // Text Search input
+    searchInputRingDesign();
+    
+
+    I.click('#ring_list_section .ring_item .ring_detail_link');
 
     // 20% Deposit Available
-    // I.click('#express_job_option .pink_checkbox_box_legend a');
-    // I.switchToNextTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/deposit');
-    // I.closeCurrentTab();
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
+    I.click('#express_job_option .pink_checkbox_box_legend a');
+    I.switchToNextTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/deposit');
+    I.closeCurrentTab();
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/');
 
     // Filters right
     // I.see('#setting_video_tag');
