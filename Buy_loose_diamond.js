add046cf7e8afc3ca8598e6023d6a3a4948caf38
Jose <jsissame3484@universidadean.edu.co>
2022-09-13 15:37:22 -0500

    Ending buy loose diamond

--- Buy_loose_diamond.js
+++ Buy_loose_diamond.js
@@ -211,23 +211,23 @@ Scenario('Buy a loose diamond', ({ I }) => {
     I.seeInCurrentUrl("/engagement-ring/create/diamond");
 
     // CHECKING RING GUIDE BAR
-    // I.forceClick('Browse settings');
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/ring');
-    // I.forceClick('Browse diamonds');
-    // I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/diamond');
+    I.forceClick('Browse settings');
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/ring');
+    I.forceClick('Browse diamonds');
+    I.seeInCurrentUrl('https://novitadiamonds.com/engagement-ring/create/diamond');
 
     // CHECKING MAIN FILTERS
     //------------------------------------------------------------------------------
-    // checkShape();
-    // checkCarat();
-    // checkColour();
-    // checkPrice();
-    // checkCut();
-    // checkClarity();
+    checkShape();
+    checkCarat();
+    checkColour();
+    checkPrice();
+    checkCut();
+    checkClarity();
 
     // RESET FILTERS
     //------------------------------------------------------------------------------
-    // I.click('//*[@id="search_form"]/div[5]/a[2]');
+    I.click('//*[@id="search_form"]/div[5]/a[2]');
 
     // BUTTON ADVANCED FILTERS
     //------------------------------------------------------------------------------
@@ -236,33 +236,33 @@ Scenario('Buy a loose diamond', ({ I }) => {
 
     // CHECKING ADVANCED FILTERS
     //------------------------------------------------------------------------------
-    // checkPolish();
-    // for (let i = 0; i < params.reports.length; i++) {
-    //     I.click(params.reports[i], `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i+1}) label`);
-    //     checkReport(params.reports[i]);
-    //     I.click(params.reports[i], `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i+1}) label`);
-    // }
-    // checkSymmetry();
-    // checkRatio();
+    checkPolish();
+    for (let i = 0; i < params.reports.length; i++) {
+        I.click(params.reports[i], `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i+1}) label`);
+        checkReport(params.reports[i]);
+        I.click(params.reports[i], `#advanced_filters_content .diamond_filter_certificate_content ul li:nth-child(${i+1}) label`);
+    }
+    checkSymmetry();
+    checkRatio();
 
     // RESET FILTERS
     //------------------------------------------------------------------------------
-    // I.click('//*[@id="search_form"]/div[5]/a[2]');
+    I.click('//*[@id="search_form"]/div[5]/a[2]');
 
     // SORT RESULTS
     //------------------------------------------------------------------------------
-    // I.click('#price_table_header_img');
-    // sortByPrice('higher');
-    // I.click('#price_table_header_img');
-    // sortByPrice('smaller');
-    // I.click('#carat_table_header');
-    // sortByCarat('higher');
-    // I.click('#carat_table_header');
-    // sortByCarat('smaller');
-    // I.click('#report_table_header');
-    // sortByReport('higher');
-    // I.click('#report_table_header');
-    // sortByReport('smaller');
+    I.click('#price_table_header_img');
+    sortByPrice('higher');
+    I.click('#price_table_header_img');
+    sortByPrice('smaller');
+    I.click('#carat_table_header');
+    sortByCarat('higher');
+    I.click('#carat_table_header');
+    sortByCarat('smaller');
+    I.click('#report_table_header');
+    sortByReport('higher');
+    I.click('#report_table_header');
+    sortByReport('smaller');
 
     // SELECT A DIAMOND
     I.click('//*[@id="body_table_results"]/tr[1]/td[10]/a');
