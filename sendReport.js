6184e924a876b99e75ff57a75967665de568a1aa
Jose Sissa <jsissame3484@universidadean.edu.co>
2022-12-12 22:13:45 -0500

    Merge with reportTest branch, report is generate and I can to execute function after test run

--- sendReport.js
+++ sendReport.js
@@ -1,11 +1,10 @@
 const sendReport = (report) => {
-    const me = 'Cool, one of the workers have finished'
     const suites = report.results[0].suites
     for (const elem of suites) {
       const title = elem.tests[0].title
       const state = elem.tests[0].state
       const err = elem.tests[0].err.message
-      console.log({me, title, state, err});
+      console.log({ title, state, err });
     }
 }
 
