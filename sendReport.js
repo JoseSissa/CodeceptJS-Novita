966f04c9d5e9c4ab801847c294d0c4854d11b96d
Jose Sissa <jsissame3484@universidadean.edu.co>
2022-12-12 22:42:58 -0500

    building the mail

--- sendReport.js
+++ sendReport.js
@@ -1,11 +1,43 @@
+const nodemailer = require('nodemailer')
+
+const enviarEmail = async (res) => {
+
+    const config = {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: "rafaelsissa@gmail.com",
+            pass: 'jfjzxkgjyuzljypr'
+        }
+    }
+
+    const message = {
+        from: "rafaelsissa@gmail.com",
+        to: "jose.sissa@gravital.co",
+        subject: "CORREO DE PRUEBA",
+        text: `Reporte de los test: ${res}`
+    }
+
+    const transport = nodemailer.createTransport(config)
+
+    const info = await transport.sendMail(message)
+
+    console.log(info);
+}
+enviarEmail()
+
 const sendReport = (report) => {
-    const me = 'Cool, one of the workers have finished'
     const suites = report.results[0].suites
+    let res = []
     for (const elem of suites) {
-      const title = elem.tests[0].title
-      const state = elem.tests[0].state
-      const err = elem.tests[0].err.message
-      console.log({me, title, state, err});
+        const state = elem.tests[0].state
+        if(state === 'failed') {
+            const title = elem.tests[0].title
+            const err = elem.tests[0].err.message
+            res.push({ title, state, err });
+            console.log({ title, state, err });
+        }
+        enviarEmail(res)
     }
 }
 
